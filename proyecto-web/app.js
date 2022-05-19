3be77c8c88d8ebee53e1e81f28eb25267c614c8b
jose-g-cortez-m <josecortez1991_@hotmail.com>
2022-05-19 13:36:15 -0500

    eventos de formulario

--- proyecto-web/app.js
+++ proyecto-web/app.js
@@ -1,6 +1,26 @@
-const formulario = document.getElementById('form');
+/*const formulario = document.getElementById('form');
 formulario.addEventListener('submit', function(e) {
     e.preventDefault;
     let mail = document.getElementById('email-form').value;
     console.log(mail);
-});
+});*/
+/* eventos especiales
+const formulario = document.getElementById('form')
+const checkbox = document.getElementById(activador)
+checkbox, addEventListener('change', e => {
+    if (e.target.checked) {
+        console.log('solicitud aceptada')
+    } else {
+        console.log('active la solicitud por favor')
+    }
+}); */
+/*Eventos de formulario*/
+const video_mp4 = document.getElementById('video'),
+    boton_play = document.getElementById('play'),
+    boton_pause = document.getElementById('pause');
+boton_play.addEventListener('click', () => {
+    video_mp4.play()
+})
+boton_pause.addEventListener('click', () => {
+    video_mp4.pause()
+})
